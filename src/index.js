858d23bf9b23f04697b0d1496d72fc4731bdc229
Keshav Gupta <gupta.keshav1304@gmail.com>
2024-06-04 13:49:57 +0530

    add standards for api response and api error and establish a wrapper function in asynchandler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is runnign on port : ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("Mongodb connect failed!!",err);
+})
 
